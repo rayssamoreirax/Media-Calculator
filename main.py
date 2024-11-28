bdcf4c21eae27bde1c93a8d08b2771322a3e0c25
Kelvin <kelvin.silva@telefonica.com>
2024-11-28 10:06:39 -0300

    mudando para float as medias recebidas

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-number_1 = int (input("Digite o primeiro número:"))
-number_2 = int (input("Digite o segundo número:"))
-number_3 = int (input("Digite o terceiro número:"))
+number_1 = float(input("Digite o primeiro número:"))
+number_2 = float(input("Digite o segundo número:"))
+number_3 = float(input("Digite o terceiro número:"))
 
 media = (number_1) + (number_2) + (number_3) /3
 print(f"A média de {number_1}, {number_2} e {number_3} 5é: {media}") 
